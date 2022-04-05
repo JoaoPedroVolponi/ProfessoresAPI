9eb1d63eff71e447a98ef8f8278ab10690c318b4
João Pedro Rosada Volponi <jpvrosada@hotmail.com>
2022-04-05 16:00:08 -0500

    Removendo / editando professores da API

--- ProfessoresAPI/ListarProfessoresTableViewController.swift
+++ ProfessoresAPI/ListarProfessoresTableViewController.swift
@@ -3,106 +3,142 @@ import UIKit
 class ListarProfessoresTableViewController: UITableViewController {
     
     var professores : [ProfessorModel] = []
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        print("Professores")
+        
         if let url = URL(string: "https://cors.grandeporte.com.br/cursos.grandeporte.com.br:8080/professores"){
             
-            let tarefa = URLSession.shared.dataTask(with: url) {
+            let tarefa = URLSession.shared.dataTask(with: url){
                 (dados, requisicao, erro) in
-                
-                if erro == nil{
-                    print("Dados capturados da API com Sucesso!")
-                    
-                    let jDecoder = JSONDecoder()
-                    
-                    if let dadosRetornados = dados{
+                                           
+                    if erro == nil{
+                        print("Dados capturados da API com sucesso")
+                        //print(dados)
                         
-                        do{
-                            self.professores = try
-                            jDecoder.decode([ProfessorModel].self, from: dadosRetornados)
-                            //print(self.professores)
-                            self.tableView.reloadData()
-                        }
-                        catch{
-                            print("Erro ao converter Professor Model")
+                        let jDecoder = JSONDecoder()
+                        
+                        if let dadosRetornados = dados{
+                            do {
+                                self.professores  = try jDecoder.decode([ProfessorModel].self, from: dadosRetornados)
+                                //print(self.professores)
+                                self.tableView.reloadData()
+                            }
+                            catch {
+                                print("Erro ao converter Professor Model")
+                            }
                         }
                     }else{
                         print("Erro ao consultar a API")
                     }
-                }
             }
             tarefa.resume()
         }
     }
-    
+
     // MARK: - Table view data source
-    
+
     override func numberOfSections(in tableView: UITableView) -> Int {
-        
+        // #warning Incomplete implementation, return the number of sections
         return 1
     }
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      
+        // #warning Incomplete implementation, return the number of rows
         return professores.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        cell.textLabel?.text = self.professores[indexPath.row].nome
+        
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var teste = storyboard?.instantiateViewController(withIdentifier: "testestory") as! ViewController
+        teste.teste1 = professores[indexPath.row]
+        teste.index = professores[indexPath.row].id!
+        self.navigationController?.pushViewController(teste, animated: true)
     }
     
     
-     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-         
-     let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-     
-         cell.textLabel?.text = self.professores[indexPath.row].nome
-     
-     return cell
-     }
-     
     
+
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // pegar o id (API) que queremos deletar
+            let id = professores[indexPath.row].id!
+            // montar a URL que devemos fazer a requisicao
+            let urlStr = "https://cors.grandeporte.com.br/cursos.grandeporte.com.br:8080/professores/\(id)"
+            let url = URL(string :urlStr)!
+            
+            
+            var requisicao = URLRequest(url: url)
+            requisicao.httpMethod = "DELETE"
+            
+            requisicao.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            
+            // criar tarefa assincrona que permite fazer a requisicao a API
+            let tarefa = URLSession.shared.dataTask(with: requisicao){ (dados, resposta, erro) in
+                //senao houver erros, removemos o profesor do array e depois da tabela (tela)
+                if (erro == nil){
+                    print("Professor removido com sucesso")
+                    self.professores.remove(at: indexPath.row)
+                    // Delete the row from the data source
+                    tableView.deleteRows(at: [indexPath], with: .fade)
+                }
+                else{
+                    print("Erro ao remover o professor")
+                }
+                
+            }
+            tarefa.resume()
+            
+        }
+    }
+    
+
     /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
     /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
     /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
+
--- ProfessoresAPI/ViewController.swift
+++ ProfessoresAPI/ViewController.swift
@@ -1,30 +1,59 @@
 import UIKit
 
+
 class ViewController: UIViewController {
+    var teste1 = ProfessorModel()
+    var index = -1
     
     @IBOutlet weak var txtEmail: UITextField!
     @IBOutlet weak var txtNome: UITextField!
     @IBAction func buttonAdd(_ sender: Any)  {
+        var requisicao : URLRequest
         
-        let url = URL(string :"https://cors.grandeporte.com.br/cursos.grandeporte.com.br:8080/professores")!
-        
-        var requisicao = URLRequest(url: url)
-        requisicao.httpMethod = "POST"
-        
-        requisicao.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        let professor = ProfessorModel()
-        professor.nome = txtNome.text
-        professor.email = txtEmail.text
-        
-        let encoder = JSONEncoder()
-    
-        // Conversao e atribuir JSON para o corpo da requisicao
-        do {
-            requisicao.httpBody = try encoder.encode(professor)
-        }
-        catch{
-            print("Erro ao converter professor")
+        if !(index >= 0){
+            
+            
+            let url = URL(string :"https://cors.grandeporte.com.br/cursos.grandeporte.com.br:8080/professores/")!
+            
+            requisicao = URLRequest(url: url)
+            requisicao.httpMethod = "POST"
+            
+            requisicao.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            
+            let professor = ProfessorModel()
+            professor.nome = txtNome.text
+            professor.email = txtEmail.text
+            
+            let encoder = JSONEncoder()
+            
+            // Conversao e atribuir JSON para o corpo da requisicao
+            do {
+                requisicao.httpBody = try encoder.encode(professor)
+            }
+            catch{
+                print("Erro ao converter professor")
+            }
+            // editar
+        }else{
+            
+            let url = URL(string: "https://cors.grandeporte.com.br/cursos.grandeporte.com.br:8080/professores/" + String (teste1.id!))!
+            
+            requisicao = URLRequest(url: url)
+            requisicao.httpMethod = "PATCH"
+            requisicao.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            
+            let professor = ProfessorModel()
+            professor.nome = txtNome.text
+            professor.email = txtEmail.text
+            
+            let encoder = JSONEncoder()
+            
+            do {
+                requisicao.httpBody = try encoder.encode(professor)
+            }
+            catch{
+                print("Erro ao converter professor")
+            }
         }
         
         // criar a tarefa assincrona que vai fazer a requisicao
@@ -44,9 +73,15 @@ class ViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if(index >= 0){
+            txtNome.text = teste1.nome
+            txtEmail.text = teste1.email
+        }
+        
+        
     }
-
-
+    
+    
 }
 
